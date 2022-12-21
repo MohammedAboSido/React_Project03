1eee363428761f4467910d477b97fdf5fe6a4689
Mohammed A. Abo Sido <abosido2@gmail.com>
2022-12-21 14:29:06 +0200

    Responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { navbarData } from "./Data";
 import style from "./style.module.css";
 
 export default function Navbar() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked((prevstate) => {
+      return !prevstate;
+    });
+  };
   return (
     <>
       <nav className={style.navbarItems}>
         <h1 className={style.navbarLogo}>Trippy</h1>
-        <ul className={style.navMenu}>
+        <div className={style.menuIcons} onClick={handleClick}>
+          <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul
+          className={
+            clicked ? ` ${style.navMenuActive} ${style.navMenu}` : style.navMenu
+          }
+        >
           {navbarData.map((item) => {
             return (
               <li key={item.id}>
